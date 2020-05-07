0ddbf5e88183d4a34bd375e085ecf1e42691d056
chigozieorunta <chigozieorunta@yahoo.com>
2020-05-07 22:20:46 +0100

    updated states for Toggle component

--- src/components/toggle/Toggle.js
+++ src/components/toggle/Toggle.js
@@ -5,6 +5,9 @@ const toggleOff = {
   height: 20,
   borderRadius: 100,
   backgroundColor: "#D1D1D1",
+  display: "inline-block",
+  transition: "all 0.3s",
+  position: "relative",
 };
 
 const toggleOn = {
@@ -12,18 +15,55 @@ const toggleOn = {
   height: 20,
   borderRadius: 100,
   backgroundColor: "#F00",
+  display: "inline-block",
+  transition: "all 0.3s",
+  position: "relative",
 };
+
+const toggleChildOff = {
+  width: 16,
+  height: 16,
+  borderRadius: "100%",
+  position: "absolute",
+  top: 2,
+  left: 2,
+  transition: "all 0.3s",
+  backgroundColor: "#FFF",
+};
+
+const toggleChildOn = {
+  width: 16,
+  height: 16,
+  borderRadius: "100%",
+  position: "absolute",
+  top: 2,
+  left: 22,
+  transition: "all 0.3s",
+  backgroundColor: "#FFF",
+};
+
+let bool = true;
 
 const Toggle = () => {
   const [toggleState, setToggleState] = useState(toggleOff);
+  const [toggleChildState, setToggleChildState] = useState(toggleChildOff);
 
   const toggleControl = () => {
-    setToggleState(toggleOn);
+    if (bool) {
+      setToggleState(toggleOn);
+      setToggleChildState(toggleChildOn);
+    } else {
+      setToggleState(toggleOff);
+      setToggleChildState(toggleChildOff);
+    }
+    bool = !bool;
   };
 
   return (
     <>
-      <div style={toggleState} onClick={toggleControl}></div>
+      <div style={toggleState} onClick={toggleControl}>
+        <div style={toggleChildState}></div>
+      </div>
       <input type="hidden" />
     </>
   );
