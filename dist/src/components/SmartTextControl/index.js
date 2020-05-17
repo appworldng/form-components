56addfb92a9814dc1603644a17860005d8381c44
chigozieorunta <chigozieorunta@yahoo.com>
2020-05-16 23:40:55 -0400

    recent build

---
+++ dist/src/components/SmartTextControl/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var SmartTextControl = function SmartTextControl() {
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null);
+};
+
+var _default = SmartTextControl;
+exports.default = _default;
+
+//# sourceMappingURL=index.js.map
