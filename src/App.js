5c4e1ce827d62500d27347fe69afbcdda8c13a1e
chigozieorunta <chigozieorunta@yahoo.com>
2020-05-17 01:01:00 -0400

    changed app components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ToggleControl from "./components/ToggleControl";
+import SliderControl from "./components/SliderControl";
+import KnobControl from "./components/KnobControl";
+import SmartTextControl from "./components/SmartTextControl";
+
+const styles = {
+  textAlign: "center",
+  display: "grid",
+  gridTemplateColumns: "repeat(4, 1fr)",
+  padding: "4em",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section style={styles}>
+        <div>
+          <ToggleControl />
+        </div>
+        <div>
+          <SliderControl />
+        </div>
+        <div>
+          <KnobControl />
+        </div>
+        <div>
+          <SmartTextControl />
+        </div>
+      </section>
+    </>
   );
 }
 
